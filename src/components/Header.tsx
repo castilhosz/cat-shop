34ef14283d5e57715788d4018e95223337fcd909
Arthur Castilhos <arthurcastilhos@gmail.com>
2022-02-11 10:16:09 -0300

    feat: active nav style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,73 +6,27 @@ import { useRouter } from 'next/router';
 import HeaderLogin from './HeaderLogin';
 
 export default function Header() {
-  const route = useRouter()
+  const router = useRouter();
 
-  if (route.pathname = '/') {
-    return (
-      <div className={styles.main}>
-        <HeaderLogin />
-        <div className={styles.container}>
-          <p>&quot;) bubs_shop</p>
+  return (
+    <div className={styles.main}>
+      <HeaderLogin />
+      <div className={styles.container}>
+        <p>&quot;) bubs_shop</p>
 
-          <nav>
-            <ul>
-              <li className={styles.active}>
-                <Link href="/"><a>FEED</a></Link>
-              </li>
-              <li>
-                <Link href="/estoque"><a>EM ESTOQUE</a></Link>
-              </li>
-            </ul>
-          </nav>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/"><a id={router.pathname == '/' ? "active" : ""}>FEED</a></Link>
+            </li>
+            <li>
+              <Link href="/estoque"><a id={router.pathname == '/estoque' ? "active" : ""}>EM ESTOQUE</a></Link>
+            </li>
+          </ul>
+        </nav>
 
-          <div />
-        </div>
+        <div />
       </div>
-    )
-  } else if (route.pathname = '/estoque') {
-    return (
-      <div className={styles.main}>
-        <HeaderLogin />
-        <div className={styles.container}>
-          <p>&quot;) bubs_shop</p>
-
-          <nav>
-            <ul>
-              <li>
-                <Link href="/"><a>FEED</a></Link>
-              </li>
-              <li className={styles.active}>
-                <Link href="/estoque"><a>EM ESTOQUE</a></Link>
-              </li>
-            </ul>
-          </nav>
-
-          <div />
-        </div>
-      </div>
-    )
-  } else {
-    return (
-      <div className={styles.main}>
-        <HeaderLogin />
-        <div className={styles.container}>
-          <p>&quot;) bubs_shop</p>
-
-          <nav>
-            <ul>
-              <li>
-                <Link href="/"><a>FEED</a></Link>
-              </li>
-              <li>
-                <Link href="/estoque"><a>EM ESTOQUE</a></Link>
-              </li>
-            </ul>
-          </nav>
-
-          <div />
-        </div>
-      </div>
-    )
-  }
+    </div>
+  )
 }
