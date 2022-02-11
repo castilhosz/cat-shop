5568fc697d4e17807ba7e20bace8ff620f01a9d9
Arthur Castilhos <arthurcastilhos@gmail.com>
2022-02-11 09:48:12 -0300

    feat: identify actual page and set className active

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,78 @@
 import React from 'react';
 import styles from '../styles/components/Header.module.css';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import HeaderLogin from './HeaderLogin';
 
 export default function Header() {
-  return (
-    <div className={styles.main}>
-      <HeaderLogin />
-      <div className={styles.container}>
-        <p>&quot;) bubs_shop</p>
+  const route = useRouter()
 
-        <nav>
-          <ul>
-            <li>
-              <Link href="/"><a>FEED</a></Link>
-            </li>
-            <li>
-              <Link href="/estoque"><a>EM ESTOQUE</a></Link>
-            </li>
-          </ul>
-        </nav>
+  if (route.pathname = '/') {
+    return (
+      <div className={styles.main}>
+        <HeaderLogin />
+        <div className={styles.container}>
+          <p>&quot;) bubs_shop</p>
 
-        <div />
+          <nav>
+            <ul>
+              <li className={styles.active}>
+                <Link href="/"><a>FEED</a></Link>
+              </li>
+              <li>
+                <Link href="/estoque"><a>EM ESTOQUE</a></Link>
+              </li>
+            </ul>
+          </nav>
+
+          <div />
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else if (route.pathname = '/estoque') {
+    return (
+      <div className={styles.main}>
+        <HeaderLogin />
+        <div className={styles.container}>
+          <p>&quot;) bubs_shop</p>
+
+          <nav>
+            <ul>
+              <li>
+                <Link href="/"><a>FEED</a></Link>
+              </li>
+              <li className={styles.active}>
+                <Link href="/estoque"><a>EM ESTOQUE</a></Link>
+              </li>
+            </ul>
+          </nav>
+
+          <div />
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className={styles.main}>
+        <HeaderLogin />
+        <div className={styles.container}>
+          <p>&quot;) bubs_shop</p>
+
+          <nav>
+            <ul>
+              <li>
+                <Link href="/"><a>FEED</a></Link>
+              </li>
+              <li>
+                <Link href="/estoque"><a>EM ESTOQUE</a></Link>
+              </li>
+            </ul>
+          </nav>
+
+          <div />
+        </div>
+      </div>
+    )
+  }
 }
