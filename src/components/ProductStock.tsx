9c260ff0cf246d3d049292eed44259f7a68b22ec
Arthur Castilhos <arthurcastilhos@gmail.com>
2022-02-13 22:24:35 -0300

    feat: separate product from stock-page

---
+++ src/components/ProductStock.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from '../styles/components/ProductStock.module.css';
+import Link from 'next/link';
+
+export default function HeaderLogin() {
+  return (
+    <div className={styles.container}></div>
+  )
+}
