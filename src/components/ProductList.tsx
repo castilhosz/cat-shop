64ace04a1b9c9850b48fba8df23bb0ba7c064da2
Arthur Castilhos <arthurcastilhos@gmail.com>
2022-02-16 14:39:24 -0300

    fix: use localiamages now

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import styles from '../styles/components/ProductList.module.css';
 import products from '../../products.json';
 import Image from 'next/image';
 
 export default function ProductList() {
   return (
-    <div >
+    <div className={styles.container}>
       {products.map(e =>
         <div key={e.id}>
-          <Image src={e.img} alt="" />
+          <Image
+            src={e.img}
+            alt=""
+            height={500}
+            width={500} />
           <p>{e.subtitle}</p>
           <h1>{e.title}</h1>
         </div>
