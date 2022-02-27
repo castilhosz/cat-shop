fcfdf0f436b9fbe18c2cab6f6eef52356f0b4bfa
Arthur Castilhos <arthurcastilhos@gmail.com>
2022-02-27 18:38:29 -0300

    feat: auth html working

---
+++ src/pages/auth.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import Image from "next/image"
+import Link from "next/link"
+
+import styles from "../styles/pages/Auth.module.css"
+
+const Auth: React.FC = () => {
+  return(
+    <div className={styles["page-auth"]}>
+      <div className={styles.container}>
+        <Image
+          height={300}
+          width={300}
+          src="/favicon.png"
+        />
+        <h1>ENTRE NA SUA CONTA</h1>
+        <form>
+          <input type="email" placeholder="Endereço de e-mail"/>
+        </form>
+        <form>
+          <input placeholder="Senha"/>
+        </form>
+        <div>
+          <form>
+            <input type="checkbox" id="remember"/>
+            <label htmlFor="remember">Mantenha-me conectado</label>
+          </form>
+          <Link href="/recuperar"><a>Esqueceu sua senha?</a></Link>
+        </div>
+        <button><p>ENTRAR</p></button>
+      </div>
+    </div>
+  )
+}
+
+export default Auth
